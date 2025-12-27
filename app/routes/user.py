f07fc56ed03013772f2187032975e2564549de49
Chandu <chandumoorman@gmail.com>
2025-12-27 19:21:28 +0530

    Day 4: authentication and password hashing

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,25 +1,35 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
-from app.schemas.user import UserCreate
+from app.schemas.user import UserCreate, UserLogin
 from app.models.user import User
 from app.core.deps import get_db
+from app.core.security import hash_password, verify_password
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
+# STEP 5: SIGNUP API
+@router.post("/signup")
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    hashed_pwd = hash_password(user.password)
 
-@router.post("/")
-def create_user(user: UserCreate, db: Session = Depends(get_db)):
     new_user = User(
         username=user.username,
-        email=user.email
+        email=user.email,
+        hashed_password=hashed_pwd
     )
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+
+    return {"message": "User registered successfully"}
 
 
-@router.get("/")
-def list_users(db: Session = Depends(get_db)):
-    return db.query(User).all()
+# STEP 6: LOGIN API  👇👇👇
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        return {"error": "Invalid credentials"}
+
+    return {"message": "Login successful"}
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class UserCreate(BaseModel):
     username: str
     email: str
+    password: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
